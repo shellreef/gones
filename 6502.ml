41c811837328450eb2cb99746090c6385e1814ef
Jeff Connelly <shellreef@gmail.com>
2010-11-14 13:15:45 -0800

    Partial reading of iNES header

--- 6502.ml
+++ 6502.ml
@@ -120,6 +120,28 @@ let formatOperand mode operand =
 *)
 
 
-print_endline ((stringOfOpcode (fst (Array.get opcodeMap 0xa1 ))) ^ " " ^ (formatOperand (snd (Array.get opcodeMap 0xa1)) 123));;
+let f = open_in_bin "SMARIO.NES";;
+
+let iNes_signature = 0x4E45531A;;
+let page_size = 16384;;
+
+(* http://nesdev.parodius.com/neshdr20.txt *)
+assert (input_binary_int f == iNes_signature);;
+
+let prg_pages = input_byte f;;
+let chr_pages = input_byte f;;
+let mapper_info1 = input_byte f;;
+let mapper_info2 = input_byte f;;
+(* TODO: read extended info, bytes 8-15 *)
+seek_in f 16;;
+
+Printf.printf "%x\n" (input_byte f);;
+Printf.printf "%x\n" (input_byte f);;
+Printf.printf "%x\n" (input_byte f);;
+Printf.printf "%x\n" (input_byte f);;
+
+Printf.printf "ROM: %d, VROM: %d -- mapper info: %x %x\n" prg_pages chr_pages mapper_info1 mapper_info2;;
+
+print_endline ((stringOfOpcode (fst (Array.get opcodeMap 0xa9))) ^ " " ^ (formatOperand (snd (Array.get opcodeMap 0xa9 )) 0x40));;
 
 
