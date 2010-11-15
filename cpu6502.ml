1eba8bd651a11d3a4beda681b17d161f63c9801f
Jeff Connelly <shellreef@gmail.com>
2010-11-14 23:47:44 -0800

    Fix opcode table. http://nesdev.parodius.com/6502.txt has many errors; http://www.akk.org/~flo/6502%20OpCode%20Disass.pdf is better

--- cpu6502.ml
+++ cpu6502.ml
@@ -1,3 +1,9 @@
+(* Created:2010114
+ * By Jeff Connelly
+ *
+ * 6502 microprocessor
+ *)
+
 type opcode = ADC | AND | ASL | BCC | BCS | BEQ | BIT | BMI | BNE | BPL | BRK | BVC | 
     BVS | CLC | CLD | CLI | CLV | CMP | CPX | CPY | DEC | DEX | DEY | EOR | INC | INX | 
     INY | JMP | JSR | LDA | LDX | LDY | LSR | NOP | ORA | PHA | PHP | PLA | PLP | ROL | 
@@ -8,37 +14,44 @@ type opcode = ADC | AND | ASL | BCC | BCS | BEQ | BIT | BMI | BNE | BPL | BRK | 
 
 type addrMode = Imm | Zer | Ixz | Iyz | Abs | Inx | Iny | Pre | Pst | Imp | Acc | Ind | Rel;;
 
-(* http://nesdev.parodius.com/6502.txt *)
+(* Opcode byte to opcode and addressing mode
+Note: http://nesdev.parodius.com/6502.txt has several errors. 
+http://www.akk.org/~flo/6502%20OpCode%20Disass.pdf is more correct, notably:
+0x7d is ADC, Iny
+0x8d is STA, Abs
+0x90 is BCC, Rel
+*)
+
 let opcodeMap = [|
 (* Indexed by opcode, value is (mneumonic, addressing mode code) *)
 (* x0         x1         x2         x3         x4        x5          x6         x7   *)
 (* x8         x9         xa         xb         xc        xd          xe         xf   *)
 (BRK, Imp);(ORA, Pre);(U__, Imm);(U__, Imm);(U__, Imm);(ORA, Zer);(ASL, Zer);(U__, Imm); (* 0x *)
 (PHP, Imp);(ORA, Imm);(ASL, Acc);(U__, Imm);(U__, Imm);(ORA, Abs);(ASL, Abs);(U__, Imm); 
-(BPL, Imp);(ORA, Pst);(U__, Imm);(U__, Imm);(U__, Imm);(ORA, Ixz);(ASL, Ixz);(U__, Imm); (* 1x *)
+(BPL, Rel);(ORA, Pst);(U__, Imm);(U__, Imm);(U__, Imm);(ORA, Ixz);(ASL, Ixz);(U__, Imm); (* 1x *)
 (CLC, Imp);(ORA, Iny);(U__, Imm);(U__, Imm);(U__, Imm);(U__, Imm);(ASL, Inx);(U__, Imm); 
 (JSR, Abs);(AND, Pre);(U__, Imm);(U__, Imm);(BIT, Zer);(AND, Zer);(ROL, Zer);(U__, Imm); (* 2x *)
 (PLP, Imp);(AND, Imm);(ROL, Acc);(U__, Imm);(BIT, Abs);(AND, Abs);(ROL, Abs);(U__, Imm); 
 (BMI, Rel);(AND, Pst);(U__, Imm);(U__, Imm);(U__, Imm);(AND, Ixz);(ROL, Ixz);(U__, Imm); (* 3x *)
 (SEC, Imp);(AND, Iny);(U__, Imm);(U__, Imm);(U__, Imm);(AND, Inx);(ROL, Inx);(U__, Imm); 
-(EOR, Abs);(EOR, Pre);(U__, Imm);(U__, Imm);(U__, Imm);(EOR, Zer);(LSR, Zer);(U__, Imm); (* 4x *)
-(PHA, Imp);(EOR, Imm);(LSR, Acc);(U__, Imm);(JMP, Abs);(RTI, Imp);(LSR, Abs);(U__, Imm);
-(EOR, Inx);(EOR, Pst);(U__, Imm);(U__, Imm);(U__, Imm);(EOR, Ixz);(LSR, Ixz);(U__, Imm); (* 5x *)
+(RTI, Imp);(EOR, Pre);(U__, Imm);(U__, Imm);(U__, Imm);(EOR, Zer);(LSR, Zer);(U__, Imm); (* 4x *)
+(PHA, Imp);(EOR, Imm);(LSR, Acc);(U__, Imm);(JMP, Abs);(EOR, Abs);(LSR, Abs);(U__, Imm);
+(BVC, Rel);(EOR, Pst);(U__, Imm);(U__, Imm);(U__, Imm);(EOR, Ixz);(LSR, Ixz);(U__, Imm); (* 5x *)
 (CLI, Imp);(EOR, Iny);(U__, Imm);(U__, Imm);(U__, Imm);(U__, Imm);(LSR, Inx);(U__, Imm);
 (RTS, Imp);(ADC, Pre);(U__, Imm);(U__, Imm);(U__, Imm);(ADC, Zer);(ROR, Zer);(U__, Imm); (* 6x *)
 (PLA, Imp);(ADC, Imm);(ROR, Acc);(U__, Imm);(JMP, Ind);(U__, Imm);(ROR, Abs);(U__, Imm);
 (BVS, Rel);(ADC, Pst);(U__, Imm);(U__, Imm);(U__, Imm);(ADC, Ixz);(ROR, Ixz);(U__, Imm); (* 7x *)
-(SEI, Imp);(ADC, Iny);(U__, Imm);(U__, Imm);(U__, Imm);(U__, Imm);(ROR, Inx);(U__, Imm);
-(STA, Abs);(STA, Pre);(U__, Imm);(U__, Imm);(STY, Zer);(STA, Zer);(STX, Zer);(U__, Imm); (* 8x *)
-(DEY, Imp);(U__, Imm);(TXA, Imp);(U__, Imm);(STY, Abs);(U__, Imm);(STX, Abs);(U__, Imm);
-(STA, Inx);(STA, Pst);(U__, Imm);(U__, Imm);(STY, Ixz);(STA, Ixz);(STX, Ixz);(U__, Imm); (* 9x *)
+(SEI, Imp);(ADC, Iny);(U__, Imm);(U__, Imm);(U__, Imm);(ADC, Iny);(ROR, Inx);(U__, Imm);
+(U__, Imm);(STA, Pre);(U__, Imm);(U__, Imm);(STY, Zer);(STA, Zer);(STX, Zer);(U__, Imm); (* 8x *)
+(DEY, Imp);(U__, Imm);(TXA, Imp);(U__, Imm);(STY, Abs);(STA, Abs);(STX, Abs);(U__, Imm);
+(BCC, Rel);(STA, Pst);(U__, Imm);(U__, Imm);(STY, Ixz);(STA, Ixz);(STX, Ixz);(U__, Imm); (* 9x *)
 (TYA, Imp);(STA, Iny);(TXS, Imp);(U__, Imm);(U__, Imm);(U__, Imm);(U__, Imm);(U__, Imm);
 (LDY, Imm);(LDA, Pre);(LDX, Imm);(U__, Imm);(LDY, Zer);(LDA, Zer);(LDX, Zer);(U__, Imm); (* ax *)
 (TAY, Imp);(LDA, Imm);(TAX, Imp);(U__, Imm);(LDY, Abs);(LDA, Abs);(LDX, Abs);(U__, Imm);
 (BCS, Rel);(LDA, Pst);(U__, Imm);(U__, Imm);(LDY, Ixz);(LDA, Ixz);(LDX, Iyz);(U__, Imm); (* bx *)
 (CLV, Imp);(LDA, Iny);(TSX, Imp);(U__, Imm);(LDY, Inx);(LDA, Inx);(LDX, Iny);(U__, Imm);
 (CPY, Imm);(CMP, Pre);(U__, Imm);(U__, Imm);(CPY, Zer);(CMP, Zer);(DEC, Zer);(U__, Imm); (* cx *)
-(INY, Imp);(CMP, Imm);(DEX, Imp);(U__, Imm);(CPY, Ixz);(CMP, Abs);(DEC, Abs);(U__, Imm);
+(INY, Imp);(CMP, Imm);(DEX, Imp);(U__, Imm);(CPY, Abs);(CMP, Abs);(DEC, Abs);(U__, Imm);
 (BNE, Rel);(CMP, Pst);(U__, Imm);(U__, Imm);(U__, Imm);(CMP, Ixz);(DEC, Ixz);(U__, Imm); (* dx *)
 (CLD, Imp);(CMP, Iny);(U__, Imm);(U__, Imm);(U__, Imm);(CMP, Inx);(DEC, Inx);(U__, Imm);
 (CPX, Imm);(SBC, Pre);(U__, Imm);(U__, Imm);(CPX, Zer);(SBC, Zer);(INC, Zer);(U__, Imm); (* ex *)
---
+++ nesfile.ml
@@ -0,0 +1,48 @@
+(* Created:20101114
+ * By Jeff Connelly
+ *
+ * iNES (.nes) file reader
+ *)
+
+let read filename =
+    let f = open_in_bin filename in    (* not using extlib IO because it lacks seek *)
+
+    (* http://nesdev.parodius.com/neshdr20.txt *)
+    let signature = 0x4E45531A in
+    (*let page_size = 16384 in *)
+
+    assert (input_binary_int f == signature);
+
+    let prg_pages = input_byte f in
+    let chr_pages = input_byte f in
+    let mapper_info1 = input_byte f in
+    let mapper_info2 = input_byte f in
+
+    Printf.printf "ROM: %d, VROM: %d -- mapper info: %x %x\n" prg_pages chr_pages mapper_info1 mapper_info2;
+
+    (* TODO: read extended info, bytes 8-15 *)
+    seek_in f 16;
+
+    (* TODO: read banks *)
+
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+    print_endline (Cpu6502.readInstruction (IO.input_channel f));
+
+
+    print_endline (Cpu6502.readInstruction (IO.input_string "\xa9\x40"));;
+
