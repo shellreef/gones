597f0b971ef9670bd7ef82d1f853441e40101e2c
Jeff Connelly <shellreef@gmail.com>
2010-11-15 22:56:55 -0800

    Read extended info

--- nesfile.ml
+++ nesfile.ml
@@ -9,18 +9,20 @@ let read filename =
 
     (* http://nesdev.parodius.com/neshdr20.txt *)
     let signature = 0x1a53454e in     (* NES^Z, little endian *)
-    let page_size = 16384 in 
 
     assert (IO.read_i32 io == signature);
 
-    let prg_pages = IO.read_byte io in
-    let chr_pages = IO.read_byte io in
+    let prg_pages = IO.read_byte io in   (* ROM = PRG (program) data, 16384 bytes/page *)
+    let chr_pages = IO.read_byte io in   (* VROM = CHR (character) data, 8192 bytes/page *)
     let mapper_info1 = IO.read_byte io in
     let mapper_info2 = IO.read_byte io in
 
     Printf.printf "ROM: %d, VROM: %d -- mapper info: %x %x\n" prg_pages chr_pages mapper_info1 mapper_info2;
 
-    let ext_info = IO.really_nread io 8 in
+    let ram_pages = IO.read_byte io in   (* 8192 bytes/page *)
+    let pal_flag = IO.read_byte io in
+
+    let reserved = IO.really_nread io 6 in
 
     (* TODO: read banks *)
 
