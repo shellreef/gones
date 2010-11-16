ef773ea93a8c735b2ed40a491821d38b6ba1d6e5
Jeff Connelly <shellreef@gmail.com>
2010-11-15 22:51:34 -0800

    nesfile: use 31-bit integers for signature check, make it work

--- nesfile.ml
+++ nesfile.ml
@@ -8,10 +8,10 @@ let read filename =
     let io = IO.input_channel (open_in_bin filename) in    (* not using extlib IO because it lacks seek *)
 
     (* http://nesdev.parodius.com/neshdr20.txt *)
-    let signature = 0x4E45531Al in      (* l suffix means int32 literal *)
-    (*let page_size = 16384 in *)
+    let signature = 0x1a53454e in     (* NES^Z, little endian *)
+    let page_size = 16384 in 
 
-    assert (IO.read_real_i32 io == signature);
+    assert (IO.read_i32 io == signature);
 
     let prg_pages = IO.read_byte io in
     let chr_pages = IO.read_byte io in
@@ -20,8 +20,7 @@ let read filename =
 
     Printf.printf "ROM: %d, VROM: %d -- mapper info: %x %x\n" prg_pages chr_pages mapper_info1 mapper_info2;
 
-    (* TODO: read extended info, bytes 8-15 *)
-    let ext_info = IO.really_nread io 16 in
+    let ext_info = IO.really_nread io 8 in
 
     (* TODO: read banks *)
 
