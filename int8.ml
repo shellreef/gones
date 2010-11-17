833ae7ee7cc309182863bdd2a4e74faf5fe16deb
Jeff Connelly <shellreef@gmail.com>
2010-11-17 09:26:04 -0800

    Add 8-bit x$ and 16-bit x6375 arithmetic

--- int8.ml
+++ int8.ml
@@ -1,18 +1,32 @@
 (* Created:20101116
  * By Jeff Connelly
  *
- * 8-bit integer type
+ * 8-bit integer operations
  *)
 
 (*
+
 OCaml has int32, int64 (including literals): http://caml.inria.fr/pub/docs/manual-ocaml/manual021.html
 
 extlib has BitSet, UChar: http://ocaml-lib.sourceforge.net/doc/index.html
 
 Jane Street Core has int,int32,int32,int64/native http://www.janestreet.com/ocaml/janestreet-ocamldocs/core/index.html
 
-Bigarray has int8_unsigned_elt: http://caml.inria.fr/pub/docs/manual-ocaml/libref/Bigarray.html#TYPEint8_signed_elt
+Bigarray has int8_unsigned_elt: http://caml.inria.fr/pub/docs/manual-ocaml/libref/Bigarray.html#TYPEint8_signed_elt - but it is accessed as an int
 
 *)
 
-(* TODO *)
+(* 8-bit operations *)
+let ( +$ ) a b = (a + b) mod 256;;
+let ( -$ ) a b = (a - b) mod 256;;
+let ( *$ ) a b = (a * b) mod 256;;
+let ( /$ ) a b = (a / b) mod 256;;
+
+(* 16-bit operations *)
+let ( +$$ ) a b = (a + b) mod 65536;;
+let ( -$$ ) a b = (a - b) mod 65536;;
+let ( *$$ ) a b = (a * b) mod 65536;;
+let ( /$$ ) a b = (a / b) mod 65536;;
+
+(* TODO: bitshift << >> and rotate *)
+
