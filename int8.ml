71a9beca6d0bda5b83135377688359099768c319
Jeff Connelly <shellreef@gmail.com>
2010-11-16 22:38:40 -0800

    TODO: 8-bit data type

---
+++ int8.ml
@@ -0,0 +1,18 @@
+(* Created:20101116
+ * By Jeff Connelly
+ *
+ * 8-bit integer type
+ *)
+
+(*
+OCaml has int32, int64 (including literals): http://caml.inria.fr/pub/docs/manual-ocaml/manual021.html
+
+extlib has BitSet, UChar: http://ocaml-lib.sourceforge.net/doc/index.html
+
+Jane Street Core has int,int32,int32,int64/native http://www.janestreet.com/ocaml/janestreet-ocamldocs/core/index.html
+
+Bigarray has int8_unsigned_elt: http://caml.inria.fr/pub/docs/manual-ocaml/libref/Bigarray.html#TYPEint8_signed_elt
+
+*)
+
+(* TODO *)
