2d84a599c783c3d47169ffe0772cef46aadce2e8
Jeff Connelly <shellreef@gmail.com>
2010-11-26 19:47:47 -0800

    Add addressing mode type

--- gones/6502.go
+++ gones/6502.go
@@ -7,10 +7,11 @@ package main
 
 import "fmt"
 
-// TODO: should these map to the opcodes themselves instead of opaque integers?? Probably.
+// Operation code, a string for easy printing
 type Opcode string
-const (___="???";
+const (U__="???";
 ADC="ADC"; AND="AND"; ASL="ASL"; BCS="BCS"; BEQ="BEQ"; BIT="BIT"; BMI="BMI"; 
+BPL="BPL"; BVC="BVC"; BCC="BCC";
 BNE="BNE"; BRK="BRK"; BVS="BVS"; CLC="CLC"; CLD="CLD"; CLI="CLI"; CLV="CLV"; 
 CMP="CMP"; CPX="CPX"; CPY="CPY"; DEC="DEC"; DEX="DEX"; DEY="DEY"; EOR="EOR"; 
 INC="INC"; INX="INX"; INY="INY"; JMP="JMP"; JSR="JSR"; LDA="LDA"; LDX="LDX"; 
@@ -19,51 +20,55 @@ PLP="PLP"; ROL="ROL"; ROR="ROR"; RTI="RTI"; RTS="RTS"; SBC="SBC"; SEC="SEC";
 SED="SED"; SEI="SEI"; STA="STA"; STX="STX"; STY="STY"; TAX="TAX"; TAY="TAY"; 
 TSX="TSX"; TXA="TXA"; TXS="TXS"; TYA="TYA")
 
-// TODO: AddrMode
+// Addressing mode
+type AddrMode int
+const (Imd=1; Zpg; Zpx; Zpy; Abs; Abx; Aby; Ndx; Ndy; Imp; Acc; Ind; Rel);
 
 // Opcode and addressing mode
-type OpcodeAddrMode struct { opcode Opcode; addrmode int }
+type OpcodeAddrMode struct { opcode Opcode; addrmode AddrMode }
 
 // Indexed by opcode number, maps to decoded opcode and addressing mode
 var opcodes = [...]OpcodeAddrMode{
-// ==0==     ==1==     ==2==    ==3==     ==4==     ==5==     ==6==     ==7==
-{BRK,  9},{ORA,  7},{___,  0},{___,  0},{___,  0},{ORA,  1},{ASL,  1},{___,  0},
-{PHP,  9},{ORA,  0},{ASL, 10},{___,  0},{___,  0},{ORA,  4},{ASL,  4},{___,  0},
-{ORA,  5},{ORA,  8},{___,  0},{___,  0},{___,  0},{ORA,  2},{ASL,  2},{___,  0},
-{CLC,  9},{ORA,  6},{___,  0},{___,  0},{___,  0},{___,  0},{ASL,  5},{___,  0},
-{JSR,  4},{AND,  7},{___,  0},{___,  0},{BIT,  1},{AND,  1},{ROL,  1},{___,  0},
-{PLP,  9},{AND,  0},{ROL, 10},{___,  0},{BIT,  4},{AND,  4},{ROL,  4},{___,  0},
-{BMI, 12},{AND,  8},{___,  0},{___,  0},{___,  0},{AND,  2},{ROL,  2},{___,  0},
-{SEC,  9},{AND,  6},{___,  0},{___,  0},{___,  0},{AND,  5},{ROL,  5},{___,  0},
-{EOR,  4},{EOR,  7},{___,  0},{___,  0},{___,  0},{EOR,  1},{LSR,  1},{___,  0},
-{PHA,  9},{EOR,  0},{LSR, 10},{___,  0},{JMP,  4},{RTI,  9},{LSR,  4},{___,  0},
-{EOR,  5},{EOR,  8},{___,  0},{___,  0},{___,  0},{EOR,  2},{LSR,  2},{___,  0},
-{CLI,  9},{EOR,  6},{___,  0},{___,  0},{___,  0},{___,  0},{LSR,  5},{___,  0},
-{RTS,  9},{ADC,  7},{___,  0},{___,  0},{___,  0},{ADC,  1},{ROR,  1},{___,  0},
-{PLA,  9},{ADC,  0},{ROR, 10},{___,  0},{JMP, 12},{___,  0},{ROR,  4},{___,  0},
-{BVS, 12},{ADC,  8},{___,  0},{___,  0},{___,  0},{ADC,  2},{ROR,  2},{___,  0},
-{SEI,  9},{ADC,  6},{___,  0},{___,  0},{___,  0},{___,  0},{ROR,  5},{___,  0},
-{STA,  4},{STA,  7},{___,  0},{___,  0},{STY,  1},{STA,  1},{STX,  1},{___,  0},
-{DEY,  9},{___,  0},{TXA,  9},{___,  0},{STY,  4},{___,  0},{STX,  4},{___,  0},
-{STA,  5},{STA,  8},{___,  0},{___,  0},{STY,  2},{STA,  2},{STX,  2},{___,  0},
-{TYA,  9},{STA,  6},{TXS,  9},{___,  0},{___,  0},{___,  0},{___,  0},{___,  0},
-{LDY,  0},{LDA,  7},{LDX,  0},{___,  0},{LDY,  1},{LDA,  1},{LDX,  1},{___,  0},
-{TAY,  9},{LDA,  0},{TAX,  9},{___,  0},{LDY,  4},{LDA,  4},{LDX,  4},{___,  0},
-{BCS, 12},{LDA,  8},{___,  0},{___,  0},{LDY,  2},{LDA,  2},{LDX,  3},{___,  0},
-{CLV,  9},{LDA,  6},{TSX,  9},{___,  0},{LDY,  5},{LDA,  5},{LDX,  6},{___,  0},
-{CPY,  0},{CMP,  7},{___,  0},{___,  0},{CPY,  1},{CMP,  1},{DEC,  1},{___,  0},
-{INY,  9},{CMP,  0},{DEX,  9},{___,  0},{CPY,  2},{CMP,  4},{DEC,  4},{___,  0},
-{BNE, 12},{CMP,  8},{___,  0},{___,  0},{___,  0},{CMP,  2},{DEC,  2},{___,  0},
-{CLD,  9},{CMP,  6},{___,  0},{___,  0},{___,  0},{CMP,  5},{DEC,  5},{___,  0},
-{CPX,  0},{SBC,  7},{___,  0},{___,  0},{CPX,  1},{SBC,  1},{INC,  1},{___,  0},
-{INX,  9},{SBC,  0},{NOP,  9},{___,  0},{CPX,  2},{SBC,  4},{INC,  4},{___,  0},
-{BEQ, 12},{SBC,  8},{___,  0},{___,  0},{___,  0},{SBC,  2},{INC,  2},{___,  0},
-{SED,  9},{SBC,  6},{___,  0},{___,  0},{___,  0},{SBC,  5},{INC,  5},{___,  0},
+// Indexed by opcode, value is (mneumonic, addressing mode code) 
+// x0         x1         x2         x3         x4        x5          x6         x7   
+// x8         x9         xa         xb         xc        xd          xe         xf   
+{BRK, Imp},{ORA, Ndx},{U__, Imp},{U__, Imp},{U__, Imp},{ORA, Zpg},{ASL, Zpg},{U__, Imp}, // 0x 
+{PHP, Imp},{ORA, Imd},{ASL, Acc},{U__, Imp},{U__, Imp},{ORA, Abs},{ASL, Abs},{U__, Imp}, 
+{BPL, Rel},{ORA, Ndy},{U__, Imp},{U__, Imp},{U__, Imp},{ORA, Zpx},{ASL, Zpx},{U__, Imp}, // 1x 
+{CLC, Imp},{ORA, Aby},{U__, Imp},{U__, Imp},{U__, Imp},{U__, Imp},{ASL, Abx},{U__, Imp}, 
+{JSR, Abs},{AND, Ndx},{U__, Imp},{U__, Imp},{BIT, Zpg},{AND, Zpg},{ROL, Zpg},{U__, Imp}, // 2x 
+{PLP, Imp},{AND, Imd},{ROL, Acc},{U__, Imp},{BIT, Abs},{AND, Abs},{ROL, Abs},{U__, Imp}, 
+{BMI, Rel},{AND, Ndy},{U__, Imp},{U__, Imp},{U__, Imp},{AND, Zpx},{ROL, Zpx},{U__, Imp}, // 3x 
+{SEC, Imp},{AND, Aby},{U__, Imp},{U__, Imp},{U__, Imp},{AND, Abx},{ROL, Abx},{U__, Imp}, 
+{RTI, Imp},{EOR, Ndx},{U__, Imp},{U__, Imp},{U__, Imp},{EOR, Zpg},{LSR, Zpg},{U__, Imp}, // 4x 
+{PHA, Imp},{EOR, Imd},{LSR, Acc},{U__, Imp},{JMP, Abs},{EOR, Abs},{LSR, Abs},{U__, Imp},
+{BVC, Rel},{EOR, Ndy},{U__, Imp},{U__, Imp},{U__, Imp},{EOR, Zpx},{LSR, Zpx},{U__, Imp}, // 5x 
+{CLI, Imp},{EOR, Aby},{U__, Imp},{U__, Imp},{U__, Imp},{U__, Imp},{LSR, Abx},{U__, Imp},
+{RTS, Imp},{ADC, Ndx},{U__, Imp},{U__, Imp},{U__, Imp},{ADC, Zpg},{ROR, Zpg},{U__, Imp}, // 6x 
+{PLA, Imp},{ADC, Imd},{ROR, Acc},{U__, Imp},{JMP, Ind},{U__, Imp},{ROR, Abs},{U__, Imp},
+{BVS, Rel},{ADC, Ndy},{U__, Imp},{U__, Imp},{U__, Imp},{ADC, Zpx},{ROR, Zpx},{U__, Imp}, // 7x 
+{SEI, Imp},{ADC, Aby},{U__, Imp},{U__, Imp},{U__, Imp},{ADC, Aby},{ROR, Abx},{U__, Imp},
+{U__, Imp},{STA, Ndx},{U__, Imp},{U__, Imp},{STY, Zpg},{STA, Zpg},{STX, Zpg},{U__, Imp}, // 8x 
+{DEY, Imp},{U__, Imp},{TXA, Imp},{U__, Imp},{STY, Abs},{STA, Abs},{STX, Abs},{U__, Imp},
+{BCC, Rel},{STA, Ndy},{U__, Imp},{U__, Imp},{STY, Zpx},{STA, Zpx},{STX, Zpx},{U__, Imp}, // 9x 
+{TYA, Imp},{STA, Aby},{TXS, Imp},{U__, Imp},{U__, Imp},{U__, Imp},{U__, Imp},{U__, Imp},
+{LDY, Imd},{LDA, Ndx},{LDX, Imd},{U__, Imp},{LDY, Zpg},{LDA, Zpg},{LDX, Zpg},{U__, Imp}, // ax 
+{TAY, Imp},{LDA, Imd},{TAX, Imp},{U__, Imp},{LDY, Abs},{LDA, Abs},{LDX, Abs},{U__, Imp},
+{BCS, Rel},{LDA, Ndy},{U__, Imp},{U__, Imp},{LDY, Zpx},{LDA, Zpx},{LDX, Zpy},{U__, Imp}, // bx 
+{CLV, Imp},{LDA, Aby},{TSX, Imp},{U__, Imp},{LDY, Abx},{LDA, Abx},{LDX, Aby},{U__, Imp},
+{CPY, Imd},{CMP, Ndx},{U__, Imp},{U__, Imp},{CPY, Zpg},{CMP, Zpg},{DEC, Zpg},{U__, Imp}, // cx 
+{INY, Imp},{CMP, Imd},{DEX, Imp},{U__, Imp},{CPY, Abs},{CMP, Abs},{DEC, Abs},{U__, Imp},
+{BNE, Rel},{CMP, Ndy},{U__, Imp},{U__, Imp},{U__, Imp},{CMP, Zpx},{DEC, Zpx},{U__, Imp}, // dx 
+{CLD, Imp},{CMP, Aby},{U__, Imp},{U__, Imp},{U__, Imp},{CMP, Abx},{DEC, Abx},{U__, Imp},
+{CPX, Imd},{SBC, Ndx},{U__, Imp},{U__, Imp},{CPX, Zpg},{SBC, Zpg},{INC, Zpg},{U__, Imp}, // ex 
+{INX, Imp},{SBC, Imd},{NOP, Imp},{U__, Imp},{CPX, Zpx},{SBC, Abs},{INC, Abs},{U__, Imp},
+{BEQ, Rel},{SBC, Ndy},{U__, Imp},{U__, Imp},{U__, Imp},{SBC, Zpx},{INC, Zpx},{U__, Imp}, // fx 
+{SED, Imp},{SBC, Aby},{U__, Imp},{U__, Imp},{U__, Imp},{SBC, Abx},{INC, Abx},{U__, Imp},
 }
 
 func main() {
     fmt.Printf("Hello, world!\n")
 
-    fmt.Printf("%s", LDA);
+    fmt.Printf("%s", Abs);
 }
 
