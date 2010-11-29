54ff24ea2b3d0a4ec160a0d7490cc2dd010bb936
Jeff Connelly <shellreef@gmail.com>
2010-11-28 21:35:58 -0800

    Move main loop into Run()

--- gones/cpu6502.go
+++ gones/cpu6502.go
@@ -9,8 +9,11 @@ package cpu6502
 
 import "fmt"
 
-//import . "./dis6502"
 import . "./nesfile"
+import (
+    "bytes"
+    "./dis6502"
+)
 
 type CPU struct {
     Memory [0xffff]uint8
@@ -32,3 +35,19 @@ func (cpu *CPU) Load(cart *Cartridge) {
         panic(fmt.Sprintf("Load: PRG banks not yet supported: %d", len(cart.Prg)))
     }
 }
+
+// Start execution
+func (cpu *CPU) Run() {
+    // TODO: stop using a buffer
+    buffer := bytes.NewBuffer(cpu.Memory[0x8000:])
+    for {
+         instr, err := dis6502.ReadInstruction(buffer)
+         if err != nil {
+             break
+         }
+         // TODO: show address, but how to get file pointer on a Buffer? no Tell()
+         fmt.Printf("%s\n", instr)
+    }
+
+}
+
--- gones/main.go
+++ gones/main.go
@@ -8,10 +8,8 @@ package main
 import (
     "fmt"
     "flag"
-    "bytes"
 
     "./nesfile"
-    "./dis6502"
     "./cpu6502"
 )
 
@@ -23,15 +21,6 @@ func main() {
     cart := nesfile.Open(flag.Arg(0))
     cpu := new(cpu6502.CPU)
     cpu.Load(cart)
-
-    buffer := bytes.NewBuffer(cpu.Memory[0x8000:])
-    for {
-         instr, err := dis6502.ReadInstruction(buffer)
-         if err != nil {
-             break
-         }
-         // TODO: show address, but how to get file pointer on a Buffer? no Tell()
-         fmt.Printf("%s\n", instr)
-    }
+    cpu.Run()
 }
 
