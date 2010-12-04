a3ba853e48d87796708640a751a4747bf91d5218
Jeff Connelly <shellreef@gmail.com>
2010-12-03 21:55:32 -0800

    tracediff: compare instruction traces, ignoring disassembly

---
+++ gones/tracediff.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+# Created:20101203
+# By Jeff Connelly
+
+# Diff two instruction traces, only on the processor state (not inconsequential data representation)
+
+$ARGV[0] = "/tmp/expected";
+$ARGV[1] = "/tmp/actual";
+
+open(A, "<$ARGV[0]") || die "cannot open $ARGV[0]: $!";
+open(B, "<$ARGV[1]") || die "cannot open $ARGV[1]: $!";
+
+my $differences = 0;
+while()
+{
+    my ($a, $b);
+
+    chomp($a = <A>);
+    chomp($b = <B>);
+
+    last if !length($a) || !length($b);
+
+    # Full trace is:
+    #                   only extract beginning here:  v------------------------
+    # v------------< well, also here too
+    # C000  4C F5 C5  JMP $C5F5                       A:00 X:00 Y:00 P:24 SP:FD
+    my $state_a = substr($a, 48) . substr($a, 0, 14);
+    my $state_b = substr($b, 48) . substr($b, 0, 14);
+
+
+    if ($state_a eq $state_b) {
+        print " $a\n";   # always show line, for reference (or $b??)
+    } else {
+        # Not as expected! Log what we got instead
+        print "-$a\n+$b\n";
+        $differences += 1;
+    }
+}
+
+exit($differences ? 1 : 0);
