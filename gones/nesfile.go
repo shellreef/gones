6c45fe7eba7444ea385ed7bf10f234bae29ac352
Jeff Connelly <shellreef@gmail.com>
2010-12-05 11:00:30 -0800

    nesfile: read about half of NES 2.0 flags

--- gones/nesfile.go
+++ gones/nesfile.go
@@ -16,8 +16,10 @@ import (
 // http://wiki.nesdev.com/w/index.php/INES
 type NesfileHeader struct {
     Magic uint32
-    PrgPageCount, ChrPageCount, Flags6, Flags7, RamPageCount, Flags9, Flags10 uint8
-    Reserved [5]byte
+    PrgPageCount, ChrPageCount, Flags6, Flags7 uint8
+    // Defined by NES 2.0
+    Flags8, Flags9, Flags10, Flags11, Flags12, Flags13 uint8
+    Reserved [2]byte
 }
 
 type Mirroring string 
@@ -36,19 +38,24 @@ const (
 
 // Represents a game cartridge
 type Cartridge struct {
-    Prg []([]byte)      // Program data
-    Chr []([]byte)      // Character data
-    Platform Platform
-    MapperCode int
+    Prg []([]byte)                  // Program data
+    Chr []([]byte)                  // Character data
+    Platform Platform               // Platform to run on
+    MapperCode, SubmapperCode int   // Extra hardware inside the cart
     Mirroring Mirroring
     BatteryBacked bool
     Trainer []byte
-    HintScreen []byte     
+    HintScreen []byte
+
+    // PRG-RAM and CHR-RAM, battery-backed and not
+    RamPrgBacked, RamPrgUnback int
+    RamChrBacked, RamChrUnback int
 }
 
 const NESFILE_MAGIC = 0x1a53454e        // NES^Z
 const PRG_PAGE_SIZE = 16384
 const CHR_PAGE_SIZE = 8192 
+const PRG_RAM_PAGE_SIZE = 8192
 const TRAINER_SIZE = 512
 const HINTSCREEN_SIZE = 8192
 
@@ -66,6 +73,9 @@ func Open(filename string) (*Cartridge) {
     if header.Magic != NESFILE_MAGIC {
         panic(fmt.Sprintf("invalid nesfile signature: %x != %x", header.Magic, NESFILE_MAGIC))
     }
+
+    prgPageCount := int(header.PrgPageCount)
+    chrPageCount := int(header.ChrPageCount)
 
     // This is weird, but the mapper nibbles are spread across two bytes
     cart.MapperCode = int(header.Flags7 & 0xf0 | header.Flags6 & 0xf0 >> 4)
@@ -88,6 +98,7 @@ func Open(filename string) (*Cartridge) {
         // "512-byte trainer at $7000-$71FF (stored before PRG data)"
         cart.Trainer = readPages(buffer, TRAINER_SIZE, 1)[0]
     }
+
     
     // Flags 7: platform, NES 2.0 format
     switch header.Flags7 & 3 {
@@ -99,12 +110,50 @@ func Open(filename string) (*Cartridge) {
 
     if header.Flags7 & 0x0c == 8 {
         fmt.Printf("NES 2.0 detected\n")
-        // TODO: read flags 8-15 http://wiki.nesdev.com/w/index.php/NES_2.0
+        // Flags 8-15 http://wiki.nesdev.com/w/index.php/NES_2.0
+
+        // Mapper variant
+        cart.MapperCode |= int(header.Flags8) & 0x0f << 11
+        cart.SubmapperCode = int(header.Flags8) & 0x0f >> 4
+
+        // Upper bits of ROM size
+        prgPageCount |= int(header.Flags9) & 0x0f << 9
+        chrPageCount |= int(header.Flags9) & 0x0f >> 4 << 9
+    
+        // RAM sizes
+        cart.RamPrgBacked = 1 << 6 + (int(header.Flags10) & 0x0f)
+        cart.RamPrgUnback = 1 << 6 + (int(header.Flags10) & 0xf0 >> 4)
+        cart.RamChrBacked = 1 << 6 + (int(header.Flags11) & 0x0f)
+        cart.RamChrUnback = 1 << 6 + (int(header.Flags11) & 0xf0 >> 4)
+
+        // Special case: 0, which encodes to 64 bytes, means 0 bytes
+        if cart.RamPrgBacked == 64 { 
+            cart.RamPrgBacked = 0 
+        }
+        if cart.RamPrgUnback == 64 { 
+            cart.RamPrgUnback = 0
+        } 
+        if cart.RamChrBacked == 64 {
+            cart.RamPrgBacked = 0
+        }
+        if cart.RamChrUnback == 64 {
+            cart.RamChrUnback = 0
+        }
+
+    } else {
+        // http://wiki.nesdev.com/w/index.php/INES says "8: Size of PRG RAM in 8 KB units (Value 0 infers 8 KB for compatibility; see PRG RAM circuit)"
+        // but http://wiki.nesdev.com/w/index.php/NES_2.0 says byte 8 is mapper variant
+        if header.Flags8 == 0 {
+            cart.RamPrgUnback = PRG_RAM_PAGE_SIZE
+        } else {
+            cart.RamPrgUnback = int(header.Flags8) * PRG_RAM_PAGE_SIZE
+        }
+        // TODO: backed or unbacked?
     }
 
     // Read banks
-    cart.Prg = readPages(buffer, PRG_PAGE_SIZE, int(header.PrgPageCount))
-    cart.Chr = readPages(buffer, CHR_PAGE_SIZE, int(header.ChrPageCount))
+    cart.Prg = readPages(buffer, PRG_PAGE_SIZE, prgPageCount)
+    cart.Chr = readPages(buffer, CHR_PAGE_SIZE, chrPageCount)
 
     if cart.Platform == PlatformPlayChoice {
         // "PlayChoice-10 (8KB of Hint Screen data stored after CHR data)"
