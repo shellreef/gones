814fefd45c20d2f6c7312c0e41a6297bfe05f6f5
Jeff Connelly <shellreef@gmail.com>
2010-12-05 14:27:04 -0800

    gamegenie: unscramble codes

--- gones/gamegenie.go
+++ gones/gamegenie.go
@@ -25,22 +25,45 @@ type GameGenieCode struct {
 // Position in this string corresponds to numerical value of code
 const LETTERS = "APZLGITYEOXUKSVN"
 
-// Get Game Genie value of a letter, 0-15
-func letterToValue(letter string) (value int) {
-    value = strings.Index(LETTERS, strings.ToUpper(letter))
-    if value == -1 {
+func (c GameGenieCode) String() (string) {
+    return fmt.Sprintf("%.4X:%.2X?%.2X", c.Address, c.Value, c.Key)
+}
+
+// Get Game Genie numerical hex digit of a letter, 0-15
+func letterToDigit(letter string) (digit int) {
+    digit = strings.Index(LETTERS, strings.ToUpper(letter))
+    if digit == -1 {
         panic(fmt.Sprintf("letterToValue(%s): invalid", letter))
         // TODO: error handling
     }
-    return value
+    return digit
 }
 
 func Decode(s string) (c GameGenieCode) {
-    for _, characterCode := range s {
+    var digits [8]int
+    for i, characterCode := range s {
         letter := string(characterCode)
-
-        fmt.Printf("letter=%s, value=%d\n", letter, letterToValue(letter))
+        digits[i] = letterToDigit(letter)
     }
+
+    // Unscramble digits
+    c.Value = uint8(((digits[0]&8)<<4)+((digits[1]&7)<<4)+(digits[0]&7))
+    c.Address = uint16(((digits[3]&7)<<12)+((digits[4]&8)<<8)+((digits[5]&7)<<8)+
+            ((digits[1]&8)<<4)+((digits[2]&7)<<4)+(digits[3]&8)+(digits[4]&7))
+    if len(s) == 8 {
+        c.Value += uint8(digits[7] & 8)
+        c.Key = uint8(((digits[6]&8)<<4)+((digits[7]&7)<<4)+(digits[5]&8)+(digits[6]&7))
+        c.HasKey = true
+    } else {
+        c.Value += uint8(digits[5] & 8)
+    }
+
+    // Codes like this don't automagically go to the next line when the sixth 
+    // letter is typed. They should be 8 letters, but if you only type 6, they'll
+    // still take affect.
+    c.WantsKey = digits[2] >> 3 != 0
+
+    fmt.Printf("code=%v\n", c)
 
     return c
 }
