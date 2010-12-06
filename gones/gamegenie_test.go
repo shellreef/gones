5117d4e426f2422921f0c10b4ff1fd0993339576
Jeff Connelly <shellreef@gmail.com>
2010-12-05 19:27:15 -0800

    gamegenie_test: simplify testing, add expect()

--- gones/gamegenie_test.go
+++ gones/gamegenie_test.go
@@ -7,60 +7,48 @@ package gamegenie
 
 import (
     "testing"
-    "fmt"
 )
+
+// Fail if condition is false
+func expect(t *testing.T, f bool) {
+    if !f {
+        t.Fail()
+    }
+}
 
 // 6-letter code
 func TestDecode6(t *testing.T) {
     c := Decode("SLZPLO")
-    if c.Address != 0x1123 {
-        t.Errorf("Decode6: bad address: %.4x\n", c.Address)
-    }
-    if c.Value != 0xbd {
-        t.Errorf("Decode6: bad value: %.2x\n", c.Value)
-    }
-    if c.HasKey {
-        t.Errorf("Decode6: has key, even though is 6 letters")
-    }
-    if c.WantsKey {
-        t.Errorf("Decode6: wants key, even though shouldn't")
-    }
+    expect(t, c.Address == 0x1123)
+    expect(t, c.Value == 0xbd)
+    expect(t, !c.HasKey)
+    expect(t, !c.WantsKey)
 }
 
 // 8-letter code
 func TestDecode8(t *testing.T) {
     c := Decode("SLXPLOVS")
-    if c.Address != 0x1123 {
-        t.Errorf("Decode8: bad address: %.4x\n", c.Address)
-    }
-    if c.Value != 0xbd {
-        t.Errorf("Decode8: bad value: %.2x\n", c.Value)
-    }
-    if !c.HasKey {
-        t.Errorf("Decode8: doesn't have key, even though is 6 letters")
-    }
-    if !c.WantsKey {
-        t.Errorf("Decode8: doesn't want key, even though should")
-    }
-    if c.Key != 0xde {
-        t.Errorf("Decode8: bad key: %.2x\n", c.Key)
-    }
+    expect(t, c.Address == 0x1123)
+    expect(t, c.Value == 0xbd)
+    expect(t, c.HasKey)
+    expect(t, c.WantsKey)
+    expect(t, c.Key == 0xde)
 }
 
-// 6-letter code that should be 8-letter
+// 6-letter code that should be 8-letter. These work as expected, but
+// in the Game Genie ROM don't advance to the next entry line.
 func TestDecodeMissingKey(t *testing.T) {
     c := Decode("SLXPLO")
-    if c.HasKey {
-        t.Errorf("DecodeMissingKey: has key, even though is 6 letters")
-    }
-    if !c.WantsKey {
-        t.Errorf("DecodeMissingKey: doesn't want key, but bit 3 of byte 3 is set")
-    }
+    expect(t, !c.HasKey)
+    expect(t, c.WantsKey)
 }
 
+// 8-letter code that should be 6-letter. Game Genie won't let you
+// enter these, but we allow them
 func TestDecodeExtraKey(t *testing.T) {
     c := Decode("SLZPLOVS")
-    fmt.Printf("code=%s\n", c)
+    expect(t, c.HasKey)
+    expect(t, !c.WantsKey)
 }
 
 func TestEncode(t *testing.T) {
