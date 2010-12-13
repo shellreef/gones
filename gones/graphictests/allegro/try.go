d805afd8a55a2030b657172e8a435e00cbff0728
Jeff Connelly <shellreef@gmail.com>
2010-12-12 21:52:21 -0800

    Allego: call user main, so now the test program can execute without errors and successfully disply a window..BUT, no user Go code is executed.

--- gones/graphictests/allegro/try.go
+++ gones/graphictests/allegro/try.go
@@ -10,8 +10,12 @@ import (
     "allego5")
 
 func main() {
+    // never returns
+    allego5.AllegroMain()
+
     fmt.Printf("about to call init\n")
     fmt.Printf("init = %t\n", allego5.Init())
 
-    allego5.CreateDisplay(640, 480)
+
+    //allego5.CreateDisplay(640, 480)
 }
