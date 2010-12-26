8ac3eed263fe1adbcb71c1853a82722ac563e634
Jeff Connelly <shellreef@gmail.com>
2010-12-26 15:11:52 -0800

    cartdb xml: read more fields, but still incomplete

--- gones/src/cartdb.go
+++ gones/src/cartdb.go
@@ -10,16 +10,73 @@ import ("fmt"
         "os"
         "xml")
 
+// See schema http://bootgod.dyndns.org:7777/downloads/nesdb.xsd
+
+type PRG struct {
+    Name string "attr"
+    Size string "attr"  // like "256k", TODO: integer bytes
+    CRC string "attr"
+    SHA1 string "attr"
+}
+
+type CHR struct {
+    Name string "attr"
+    Size string "attr"  // like "256k", TODO: integer bytes
+    CRC string "attr"
+    SHA1 string "attr"
+}
+
+type VRAM struct {
+    Size string
+}
+
+type Pad struct {
+    H int
+    V int
+}
+
+type Chip struct {
+    Type string
+}
+
+type CIC struct {
+    Type string
+}
+
+type Board struct {
+    Type string "attr"
+    PCB string "attr"
+    Mapper int
+    PRG []PRG
+    CHR []CHR
+    VRAM []VRAM
+    Pad []Pad
+    Chip []Chip
+    CIC []CIC
+}
+
+type Cartridge struct {
+    System string "attr"
+    CRC string "attr"
+    SHA1 string "attr"
+    Dump string "attr"
+    Dumper string "attr"
+    DateDumped string "attr"  // TODO: date format
+    Board []Board
+}
+
 type Game struct {
     Name string "attr"
-    Altname string "attr"
+    AltName string "attr"
     Class string "attr"
     Catalog string "attr"
     Publisher string "attr"
     Developer string "attr"
     Region string "attr"
     Players string "attr"
-    Date string "attr"
+    Date string "attr"       // TODO: date format
+
+    Cartridge []Cartridge
 }
 
 type Database struct {
@@ -40,6 +97,13 @@ func main() {
     xml.Unmarshal(r, &database)
 
     for i, game := range database.Game {
-        fmt.Printf("#%d. %s\n", i, game.Name)
+        fmt.Printf("#%d. %s - %s\n", i, game.Developer, game.Name)
+        for _, cart := range game.Cartridge {
+            for _, board := range cart.Board {
+                for _, chip := range board.Chip { 
+                    fmt.Printf("\t%s\n", chip.Type)
+                }
+            }
+        }
     }
 }
