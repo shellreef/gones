183f4efd806010254cbdc33a77447f9b832c4a3b
Jeff Connelly <shellreef@gmail.com>
2010-12-26 22:49:11 -0800

    Move test data output to data/

--- gones/cmptiming.pl
+++ gones/cmptiming.pl
@@ -9,7 +9,7 @@
 # counts, so you can isolate the errors more easily.
 
 open(A, "<data/nestest.log")||die;
-open(B, "</tmp/log.actual")||die;
+open(B, "<data/nestest.log-actual")||die;
 
 $failed = 0;
 while()
