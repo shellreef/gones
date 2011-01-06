efdacbb1d9d32f07226764f76ba1807c5d7fa955
Jeff Connelly <shellreef@gmail.com>
2011-01-05 23:13:55 -0800

    Verify that all 456 games that Galoob made official Game Genie codes for are in gg2gn.csv and correctly mapped to a valid GoodNES name

--- genie/galoob/verifyin.pl
+++ genie/galoob/verifyin.pl
@@ -5,6 +5,7 @@
 # Verify all games in gamelist-gg.csv can be found in nev8.txt
 
 open(FH, "<gamelist-gg.csv") || die;
+my %galoobs;
 while(<FH>) {
     chomp;
     my ($id, $name) = split /,/, $_, 2;
@@ -19,7 +20,34 @@ while(<FH>) {
         }
     }
 
-    print "$found,$name\n";
     die "incorrect game name: $name" if !$found;
+
+    $galoobs{$name} = 1;
 }
+close(FH);
+print "gamelist-gg.csv OK\n";
+
+open(FH, "<gamelist-goodnes314.csv")||die;
+chomp(my @goodnes = <FH>);
+close(FH);
+#oh
+my %goodnes;
+$goodnes{$_} = 1 for @goodnes;
+close(FH);
+
+open(FH, "<gg2gn.csv") || die;
+my $i;
+while(<FH>) {
+    chomp;
+    my ($galoob, $goodnes) = split /\t/;
+
+    die "incorrect Galoob game name: $galoob" if !$galoobs{$galoob};
+    die "incorrect GoodNES game name: $goodnes" if !$goodnes{$goodnes};
+    $i++;
+}
+my $j = scalar keys %galoobs;
+die "missing some games from gg2gn.csv ($i != $j)" if $i != $j;
+
+print "gg2gn.csv OK\n";
+print "$i games matched\n";
 
