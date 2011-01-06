6759f90c1c155b1a0bad3c1ad019c3d6a781ed2e
Jeff Connelly <shellreef@gmail.com>
2011-01-05 20:45:58 -0800

    gg-gamelist: add missing games, but now have 458, two too many

--- genie/galoob/listgames.pl
+++ genie/galoob/listgames.pl
@@ -7,7 +7,7 @@ open(FH, "<nev8.txt")||die;
 my $i = 1;
 while(<FH>) {
     chomp;
-    if (m/Game$/ || $_ eq 'Adventures of Lolo 3(tm)') {
+    if (m/Game\s*$/ || $_ eq 'Adventures of Lolo 3(tm)') {
         my $name = $_;
         my $gameid = "";
         while (!length($gameid)) {
