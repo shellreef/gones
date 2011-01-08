a5dc150201d5fbbfb29ee1b0ad7ee61f425cba1b
Jeff Connelly <shellreef@gmail.com>
2011-01-07 22:04:20 -0800

    findsource: side-by-side comparison of database + known sources

--- genie/galoob/findsource.pl
+++ genie/galoob/findsource.pl
@@ -8,30 +8,56 @@ use strict;
 
 my %sourcefiles = %{read_sources("sources/")};
 
+# Read our lines of codes for each game
 open(DB,"<all-nev.csv")||die;
+my %ourlines;  # lines of code/intro/info keyed by game
+my %game2id;
 while(<DB>) {
     chomp;
-    my ($name, $id, $type, $no, $code, $title) = split /\t/;
+    my ($name, $id, @rest) = split /\t/;
 
-    my @results = look($name);
+    $ourlines{$name} = [] if !exists $ourlines{$name};
+    $game2id{$name} = $id;
+    push @{$ourlines{$name}}, [@rest]; 
 }
 
-sub look
-{
-    my ($match) = @_;
+for my $game (sort keys %ourlines) {
+    my @ourlines = @{$ourlines{$game}};
+    print "\n$game\n";
 
-    my $found = 0;
-    for my $file (keys %sourcefiles) {
+    my $found_count = 0;
+    for my $file (sort keys %sourcefiles) {
         my @lines = @{$sourcefiles{$file}};
-        for my $line (@lines) {
-            if (lc($match) eq lc($line)) {
-                $found = 1;
-                print "Found $match in $file\n";
+        for my $i (0..$#lines) {
+            my $line = $lines[$i];
+            if (lc($game) eq lc($line)) {   # game header line
+                ++$found_count;
+                print "\t$file\n";
+                my @theirlines;
+
+                # Read more of lines from original source data
+                my $j = 0;
+                while($i < $#lines && $j < $#ourlines) {
+                    ++$i;
+                    my $theirline = $lines[$i];
+                    next if $theirline eq $game2id{$game};      # skip game abbreviation
+                    next if $theirline =~ m/CODE/i && $theirline =~ m/KEY IN/i && $theirline =~ m/EFFECT/i;   # skip field headers
+                    
+                    my @ourfields = @{$ourlines[$j]};
+                    my $ourline = join("\t", @ourfields);
+
+                    printf "%-80s %-80s\n", $ourline, $theirline;
+
+                    ++$j;
+                }
             }
         }
     }
-    die "unable to locate $match" if !$found;
+    die "unable to locate $game" if !$found_count;
+
+
 }
+
 
 # Read all sources into a hash of arrays
 sub read_sources
