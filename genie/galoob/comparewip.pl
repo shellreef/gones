d3eaf67cf32bf462f7386443ded3018a03e30bbf
Jeff Connelly <shellreef@gmail.com>
2011-01-08 00:03:27 -0800

    comparewip: compare old and new line count

--- genie/galoob/comparewip.pl
+++ genie/galoob/comparewip.pl
@@ -4,28 +4,42 @@
 
 # Differentiate wip and all-nev.csv to find missing lines
 
-open(WIP, "<wip")||die;
-open(ALL,"<all-nev.csv")||die;
-while()
-{
-    chomp(my $wip = <WIP>);
-    chomp(my $all = <ALL>);
+open(DB,"<all-nev.csv")||die;
+my %ourlines;  # lines of code/intro/info keyed by game
+my %game2id;
+while(<DB>) {
+    chomp;
+    my ($name, $id, @rest) = split /\t/;
 
-    last if !length $wip;
-
-    my @wip = split /\t/, $wip;
-    my @all = split /\t/, $all;
-
-
-    my @comparable = @wip;
-    splice @comparable, 2, 1; # remove "filename" field so it can be compared
-
-    my $comparable = join("\t", @comparable);
-
-    if ($all ne $comparable) {
-        print "-$all\n";
-        print "+$comparable\n";
-    } else {
-        print " $all\n";
-    }
+    $ourlines{$name} = [] if !exists $ourlines{$name};
+    $game2id{$name} = $id;
+    push @{$ourlines{$name}}, [@rest]; 
 }
+
+open(DB,"<wip")||die;
+my %newlines;  # lines of code/intro/info keyed by game
+my %new_game2id;
+while(<DB>) {
+    chomp;
+    my ($name, $id, @rest) = split /\t/;
+
+    $newlines{$name} = [] if !exists $newlines{$name};
+    $new_game2id{$name} = $id;
+    push @{$newlines{$name}}, [@rest]; 
+}
+
+my $ourcount = scalar keys %ourlines;
+my $newcount = scalar keys %newlines;
+die "missing games: $ourcount != $newcount" if $ourcount != $newcount;
+
+for my $game (sort keys %ourlines) 
+{
+    my @ourlines = @{$ourlines{$game}};
+    my @newlines = @{$newlines{$game}};
+
+    my $delta = @newlines - @ourlines;
+    printf "%-80s %2d -> %2d    (%+2d)\n", $game, scalar(@ourlines), scalar(@newlines), $delta;
+
+    #splice @comparable, 2, 1; # remove "filename" field so it can be compared
+}
+
