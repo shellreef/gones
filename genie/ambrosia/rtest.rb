c530a9d9b632b2ba7ac12d35863140d10c36202f
Jeff Connelly <shellreef@gmail.com>
2011-01-29 17:24:47 -0800

    Ambrosia: Non-working example of to_node

--- genie/ambrosia/rtest.rb
+++ genie/ambrosia/rtest.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 require 'ambrosia'
 
+# Example of to_s
 class Person
     attr_accessor :first, :last, :aka
 
@@ -15,6 +16,22 @@ class Person
     end
 end
 
+# Example of to_node
+class Technology 
+    attr_accessor :name, :language, :url
+
+    def initialize(name, language, url)
+        @name = name
+        @language = language
+        @url = url
+    end
+
+    def to_node(root)
+        node = Nokogiri::XML::Node.new("span", root)
+        node.content = name
+    end
+end
+
 data = {
     :x => "Hello, <script>world", 
     :item => [1,2,3], 
@@ -25,6 +42,12 @@ data = {
     :link2 => A("another link", {:href => "http://example.com/"}),
     :logo => A({:src => "http://upload.wikimedia.org/wikipedia/commons/3/3c/Ambrosia_salad.jpg"}),
     :person => Person.new("Yukihiro", "Matsumoto", ["Matz", "松本行弘", "まつもとゆきひろ"]),
+    :system => [
+        Technology.new("Ambrosia", "Ruby", nil),
+        Technology.new("Amrita", "Ruby", "http://amrita.sourceforge.jp/"),
+        Technology.new("HTML::Seamstress", "Perl", "http://search.cpan.org/~tbone/HTML-Seamstress-5.0h/lib/HTML/Seamstress.pod"),
+        Technology.new("All push-style", "Various", "http://www.perlmonks.org/?node_id=674225"),
+        ]
     }
 
 puts ambrosia(<<HTML, data)
@@ -41,6 +64,9 @@ puts ambrosia(<<HTML, data)
 <hr>
 
 <p>Ruby was created by <u id="person"></u>
+
+<p>A few templating systems:
+<div id="system"></div>
 
 HTML
 
