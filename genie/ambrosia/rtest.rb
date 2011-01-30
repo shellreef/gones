b47e8bc2890dab9c96e4cb0e797ef876712a7ad7
Jeff Connelly <shellreef@gmail.com>
2011-01-29 17:25:05 -0800

    Ambrosia: Ruby: revert

--- genie/ambrosia/rtest.rb
+++ genie/ambrosia/rtest.rb
@@ -16,22 +16,6 @@ class Person
     end
 end
 
-# Example of to_node
-class Technology 
-    attr_accessor :name, :language, :url
-
-    def initialize(name, language, url)
-        @name = name
-        @language = language
-        @url = url
-    end
-
-    def to_node(root)
-        node = Nokogiri::XML::Node.new("span", root)
-        node.content = name
-    end
-end
-
 data = {
     :x => "Hello, <script>world", 
     :item => [1,2,3], 
@@ -42,12 +26,6 @@ data = {
     :link2 => A("another link", {:href => "http://example.com/"}),
     :logo => A({:src => "http://upload.wikimedia.org/wikipedia/commons/3/3c/Ambrosia_salad.jpg"}),
     :person => Person.new("Yukihiro", "Matsumoto", ["Matz", "松本行弘", "まつもとゆきひろ"]),
-    :system => [
-        Technology.new("Ambrosia", "Ruby", nil),
-        Technology.new("Amrita", "Ruby", "http://amrita.sourceforge.jp/"),
-        Technology.new("HTML::Seamstress", "Perl", "http://search.cpan.org/~tbone/HTML-Seamstress-5.0h/lib/HTML/Seamstress.pod"),
-        Technology.new("All push-style", "Various", "http://www.perlmonks.org/?node_id=674225"),
-        ]
     }
 
 puts ambrosia(<<HTML, data)
@@ -64,9 +42,6 @@ puts ambrosia(<<HTML, data)
 <hr>
 
 <p>Ruby was created by <u id="person"></u>
-
-<p>A few templating systems:
-<div id="system"></div>
 
 HTML
 
